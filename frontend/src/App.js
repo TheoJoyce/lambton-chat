595518e1e10353c2e258d24e5de39658723d850d
damonwark <93676651+damonwark@users.noreply.github.com>
2022-03-21 12:59:34 -0400

    Complet Login
    The log in for the front end is now complet and waiting on the back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,89 @@
-import logo from './logo.svg';
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import React, {useState, useEffect} from "react";
 
-class App extends Component {
-  render() {
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// We use Route in order to define the different routes of our application
+import { Routes, Route, Link} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AuthService from "./services/auth.service";
+import Login from "./components/login.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import User from "./components/user.component";
+
+
+const App = () => {
+
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
+  return (
+   
+    <div>   
+              <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+           
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+            </div>
+          )}
+        </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/profile" element={<Profile/>} />
+          <Route exact path="/user" element={<User/>} />
+        </Routes>
+      
     </div>
-  }
+      
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -17,18 +17,18 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
     this.handleLogin = this.handleLogin.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.state = {
-      username: "",
+      Email: "",
       password: "",
       loading: false,
       message: ""
     };
   }
-  onChangeUsername(e) {
+  onChangeEmail(e) {
     this.setState({
-      username: e.target.value
+      Email: e.target.value
     });
   }
   onChangePassword(e) {
@@ -44,7 +44,7 @@ export default class Login extends Component {
     });
     this.form.validateAll();
     if (this.checkBtn.context._errors.length === 0) {
-      AuthService.login(this.state.username, this.state.password).then(
+      AuthService.login(this.state.Email, this.state.password).then(
         () => {
           this.props.history.push("/profile");
           window.location.reload();
@@ -85,13 +85,13 @@ export default class Login extends Component {
             }}
           >
             <div className="form-group">
-              <label htmlFor="username">Username</label>
+              <label htmlFor="Email">Email</label>
               <Input
                 type="text"
                 className="form-control"
-                name="username"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
+                name="Email"
+                value={this.state.Email}
+                onChange={this.onChangeEmail}
                 validations={[required]}
               />
             </div>
