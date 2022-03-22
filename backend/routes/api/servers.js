eca86183d5c144f697beb06cada84c240c1d98b8
Theo Joyce <theo@theojoyce.com>
2022-03-22 18:25:09 -0400

    Implemented authentication middleware

--- backend/routes/api/servers.js
+++ backend/routes/api/servers.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const router = express.Router();
+
 const { check } = require('express-validator');
+const {
+  authenticate,
+  authErrorHandler,
+} = require('../../middleware/authenticate');
 
 const serverController = require('../../controllers/serverController');
-
-// TODO: Add auth middleware
 
 // @route   POST api/servers/:id
 // @desc    Get server by ID
 // @access  Private
-router.get('/:id', serverController.getServerbyId);
+router.get(
+  '/:id',
+  [authenticate, authErrorHandler],
+  serverController.getServerbyId,
+);
 
 // @route   POST api/servers/create
 // @desc    Create server
 // @access  Private
-router.get(
+router.post(
   '/create',
+  [authenticate, authErrorHandler],
   check('name')
     .isLength({ min: 5, max: 64 })
     .withMessage('Name must be between 5 and 64 characters in length'),
