8c9aa6217b970af6206567f35a42b959484e8891
Theo Joyce <theo@theojoyce.com>
2022-03-22 20:00:34 -0400

    Implemented Message schema

---
+++ backend/models/Message.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const MessageSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  server: {
+    type: Schema.Types.ObjectId,
+    ref: 'Server',
+  },
+  channel: {
+    type: Schema.Types.ObjectId,
+    ref: 'Channel',
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+module.exports = Message = mongoose.model('Message', MessageSchema);
