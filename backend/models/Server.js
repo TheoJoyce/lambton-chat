183806c44e42aeb635c1b17c9bd2d1657e3f76b2
Theo Joyce <theo@theojoyce.com>
2022-03-25 12:33:47 -0400

    Enabled timestamps for all schemas

--- backend/models/Server.js
+++ backend/models/Server.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const ServerSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const ServerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
-  code: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  admin: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-});
+);
 
 module.exports = Server = mongoose.model('Server', ServerSchema);
