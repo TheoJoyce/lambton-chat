da5e72057b0e76d01d145f9d84b1051f6f34df26
Theo Joyce <theo@theojoyce.com>
2022-03-26 15:42:06 -0400

    Admin check middleware complete

---
+++ backend/middleware/adminCheck.js
@@ -0,0 +1,16 @@
+const { makeError } = require('../helpers/makeError');
+
+const adminCheck = (req, res, next) => {
+  const userID = req.user.id;
+  const adminID = req.body.admin;
+
+  if (userID.toString() === adminID.toString()) {
+    next();
+  } else {
+    res.status(401).json({
+      errors: makeError('server', 'User is not admin'),
+    });
+  }
+};
+
+module.exports = adminCheck;
