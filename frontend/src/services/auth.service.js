6f7ad11427e1300d17fa34b768604466464ad9f8
damonwark <93676651+damonwark@users.noreply.github.com>
2022-04-03 13:01:16 -0400

    Updated the users channels
    Updated the channels so that you can only see the ones that are part of your server not all of the servers.

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { createRoutesFromChildren } from "react-router-dom";
 const API_URL = `${process.env.REACT_APP_BASE_API_URL}/`
 
 class AuthService {
@@ -65,15 +66,22 @@ class AuthService {
     token = token.token;
     const id = JSON.parse(localStorage.getItem("server"));
     const serverID = id.server._id;
-    console.log (serverID);
+    const channel = [];
     axios
       .get(API_URL + "channels/all/" + serverID, { headers: {"Authorization": `Bearer ${token}`} })
       .then (res => {
-        localStorage.removeItem('channels');
-        localStorage.setItem('channels', JSON.stringify(res.data));
+        const c = JSON.parse(JSON.stringify(res.data));
+        var n = 0;
+        for (let x = c.length; x >= 0; x--){
+          const test = c[n];
+          if (test !== undefined){
+            if(test.server == serverID){
+              channel.push(c[n]);
+            }}
+          n ++;
+        }
       })
-      console.log (JSON.parse(localStorage.getItem('channels')));
-      return JSON.parse(localStorage.getItem('channels'));
+      return channel;
   }
 }
 export default new AuthService();
